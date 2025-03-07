122fc178a331e32c893f8fbca91c60610f0ccf34
qod8bop <turcenkovtimofej99228@gmail.com>
2025-03-07 20:00:27 +0300

    Add files via upload

---
+++ Htask 1.2.01.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "RU");
+
+	float num = 0.0;
+
+	cout << "Введите число: \n";
+	cin >> num;
+
+	cout << "Вы ввели: \n" << num;
+
+	return 0;
+}
