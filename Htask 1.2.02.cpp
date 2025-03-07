de5bd75124354d12861a0187ca260aeb9e6326b3
qod8bop <turcenkovtimofej99228@gmail.com>
2025-03-07 20:06:00 +0300

    Add files via upload

---
+++ Htask 1.2.02.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "RU");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string wrd;
+
+	cout << "Введите слово: \n";
+	cin >> wrd;
+
+	cout << "Вы ввели: \n" << wrd;
+
+	return 0;
+}
