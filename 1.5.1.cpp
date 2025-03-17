c03b60eb0ddaa9bdf21104a8de42bb3591d9c2e4
qod8bop <turcenkovtimofej99228@gmail.com>
2025-03-17 20:24:36 +0300

    Add files via upload

---
+++ 1.5.1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "RU");
+
+	srand(time(NULL));
+
+	short nums[11];
+
+	for (int i = 0; i < (sizeof(nums)/2) - 1; i++) {
+
+		nums[i] = 1 + rand() % 10;
+
+		cout << nums[i] << " ";
+
+	}
+
+
+	return 0;
+}
