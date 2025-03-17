f2291d3d47987991144b11e789b6446354f13c88
qod8bop <turcenkovtimofej99228@gmail.com>
2025-03-17 20:47:58 +0300

    Add files via upload

---
+++ 1.5.2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "RU");
+
+	srand(time(NULL));
+
+	short nums[11];
+	short min = 0;
+	short max = 0;
+
+
+	for (int i = 0; i < (sizeof(nums)/2) - 1; i++) {
+
+		nums[i] = 1 + rand() % 10;
+
+		cout << nums[i] << " ";
+
+	}
+
+	min = nums[0];
+	max = nums[0];
+
+
+	for (int i = 1; i < (sizeof(nums) / 2) - 1; i++) {
+
+		if (nums[i] < min) {
+			min = nums[i];
+		}
+
+		if (nums[i] > max) {
+			max = nums[i];
+		}
+
+	}
+
+
+	cout << endl << endl << "max: " << max;
+	cout << endl << endl << "min: " << min;
+	return 0;
+}
---
+++ 1.5.3.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "RU");
+
+	srand(time(NULL));
+
+	short matr[3][6];
+	short min = 0;
+	short max = 0;
+	short indMiN_i = 0;
+	short indMiN_j = 0;
+	short indMAX_i = 0;
+	short indMAX_j = 0;
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 6; j++) {
+			
+			matr[i][j] = 10 + rand() % 90;
+			cout << matr[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	min = matr[0][0];
+	max = matr[0][0];
+
+
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 6; j++) {
+		
+			if (matr[i][j] < min) {
+				min = matr[i][j];
+			}
+
+			if (matr[i][j] > max) {
+				max = matr[i][j];
+			}
+		
+		}
+	}
+
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 6; j++) {
+
+			if (matr[i][j] == min) {
+
+				indMiN_i = i ;
+				indMiN_j = j ;
+
+			}
+
+			if (matr[i][j] == max) {
+
+				indMAX_i = i ;
+				indMAX_j = j ;
+
+			}
+
+		}
+	}
+
+
+
+	cout << endl << endl << "max: " << max;
+	cout << endl << endl << "min: " << min;
+
+	cout << endl << endl;
+
+	cout << endl << endl << "Индекс минимального элемента: " << indMiN_i << " " << indMiN_j;
+	cout << endl << endl << "Индекс максимального элемента: " << indMAX_i << " " << indMAX_j;
+	
+	cout << endl << endl;
+
+	return 0;
+}
