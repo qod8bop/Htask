c763f05d195cb2baf0130c8fd08213210a442b75
qod8bop <turcenkovtimofej99228@gmail.com>
2025-03-28 13:29:28 +0300

    Add files via upload

---
+++ 1.8.1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int counter = 0;
+
+void counting_function() {
+    counter++;
+    cout << "Количество вызовов функции counting_function(): " << counter << endl;;
+}
+
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "RU");
+
+    for (int i = 0; i < 15; i++)
+    {
+        counting_function();
+    }
+}
---
+++ 1.8.2.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+namespace Calc {
+
+	namespace addi{
+		int calc(int x, int y) {
+			return x + y;
+		}
+	}
+
+	namespace subt {
+		int calc(int x, int y) {
+			return x - y;
+		}
+	}
+
+	namespace mult {
+		int calc(int x, int y) {
+			return x * y;
+		}
+	}
+
+	namespace divi {
+		int calc(int x, int y) {
+			return x / y;
+		}
+	}
+}
+
+int main() {
+
+	setlocale(LC_ALL, "RU");
+
+	int x = 6;
+	int y = 9;
+
+	cout << "x= " << x <<"  y= "<< y<< endl << endl;
+	cout << "Сложение: " << Calc::addi::calc(x, y) << endl;
+	cout << "Вычитание: " << Calc::subt::calc(x, y) << endl;
+	cout << "Умножение: " << Calc::mult::calc(x, y) << endl;
+	cout << "Деление: " << Calc::divi::calc(x, y) << endl;
+
+	return 0;
+}
