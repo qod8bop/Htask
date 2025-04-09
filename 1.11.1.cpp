6b40e2eb5a480beb3aacf8fd9d9cc3188c49aac7
qod8bop <turcenkovtimofej99228@gmail.com>
2025-04-09 09:32:02 +0300

    Add files via upload

---
+++ 1.11.1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cstring>
+#include <Windows.h>
+
+using namespace std;
+
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Введите имя: ";
+	string name;
+	cin >> name;
+	name = name + " ";
+	cout << endl;
+
+	cout << "Введите фамилию: ";
+	string sname;
+	cin >> sname;
+	cout << endl;
+
+	string intro("Здравствуйте, ");
+	string fin("!");
+
+	cout << intro + name + sname + fin;
+
+	return 0;
+}
---
+++ 1.11.2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cstring>
+#include <Windows.h>
+
+using namespace std;
+
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string mysteryword = "Говно";
+	string guess;
+
+	while (1 > 0) {
+		cout << "Угадйте слово: ";
+		cin >> guess;
+
+		if (guess == mysteryword) {
+			cout << "Правильно! Вы победили! Загаданное слово — " + mysteryword;
+			break;
+		}
+		else { cout << "Неправильно"; }
+		cout << endl;
+		cout << endl;
+	}
+
+	return 0;
+}
